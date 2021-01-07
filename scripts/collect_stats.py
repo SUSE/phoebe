0dc07cd45ca90d8f2fe160dcc2a11ab4c068e2af
Dan Čermák <dcermak@suse.com>
2021-01-07 11:30:33 +0100

    Put shebangs at the top of scripts

--- scripts/collect_stats.py
+++ scripts/collect_stats.py
@@ -1,7 +1,8 @@
-#SPDX-License-Identifier: BSD-3-Clause
-#Copyright SUSE LLC
-
 #!/usr/bin/env python3
+
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright SUSE LLC
+
 import contextlib
 import json
 import sys
