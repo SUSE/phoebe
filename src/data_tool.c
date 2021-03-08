60e4b4bb4578ae6b96a304dfdec41f2fdcea9645
Marco Varlese <marco.varlese@suse.com>
2021-03-08 14:19:42 +0100

    Formatting tidy-up

--- src/data_tool.c
+++ src/data_tool.c
@@ -1,7 +1,7 @@
 /*
 * This tool allows the migration of data across different backends.
 *
-* First of all, it can read a CSV file (with well-defined columns 
+* First of all, it can read a CSV file (with well-defined columns
 * structure) and allows the migration of data to a variety of different
 * backends:
 * 1) It can save data in binary format as per data structure used by Phoebe;
@@ -9,8 +9,7 @@
      settings.json file.
 */
 
-int main(int argc, char** argv)
-{
-	/* TO BE DEVELOPED during Hackweek */
-	return 0;
+int main(int argc, char **argv) {
+    /* TO BE DEVELOPED during Hackweek */
+    return 0;
 }
